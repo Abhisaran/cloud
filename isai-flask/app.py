933a2a268469bb0509d5601eade55e879592fc84
Abhisaran <30199540+Abhisaran@users.noreply.github.com>
2021-04-29 23:49:14 +1000

    update

--- isai-flask/app.py
+++ isai-flask/app.py
@@ -6,24 +6,29 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+app.logger.debug('app.py logging BEGIN')
 
 if __name__ != '__main__':
+    app.logger.debug('app.py !__main__ BEGIN')
     gunicorn_error_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers.extend(gunicorn_error_logger.handlers)
     app.logger.setLevel(logging.DEBUG)
 
 
 def settings():
+    app.logger.debug('app.py settings')
     return model.init_boto3()
 
 
 @app.errorhandler(404)
 def page_not_found(e):
+    app.logger.debug('app.py page_not_found')
     return redirect(url_for('login'))
 
 
 @app.route('/')
 def index():
+    app.logger.debug('app.py index')
     return redirect(url_for('login'))
 
 
@@ -35,14 +40,15 @@ def login(error):
     context = {'error': error}
     if request.method == 'POST':
         app.logger.info("login POST request: %s", request)
+        app.logger.info("login POST request: %s %s", request.form['email'], request.form['password'])
         login_validation = model.validate_user_return_session(request.form['email'], request.form['password'])
-        print("Validation : ", login_validation)
+        app.logger.info('app.py login validation: %s', login_validation)
         if login_validation:
             return redirect(url_for('main', session=login_validation))
         else:
             error = "Email or password is invalid"
             context = {'error': error}
-    print("LOGIN", context)
+    app.logger.info('app.py login context: %s', context)
     app.logger.debug('app.py login END')
     return render_template('login.html', context=context)
 
@@ -51,7 +57,7 @@ def login(error):
 @app.route('/main/<string:session>', methods=['GET', 'POST'])
 # @app.route('/main/<string:session>/<string:>', methods=['GET', 'POST'])
 def main(session):
-    app.logger.debug('app.py main BEGIN')
+    app.logger.debug('app.py main BEGIN %s', session)
     if session is None:
         app.logger.debug('app.py main END')
         return redirect(url_for('login', error='Please login first'))
@@ -74,10 +80,10 @@ def main(session):
         year = request.form['year'] if request.form['year'] else None
         artist = request.form['artist'] if request.form['artist'] else None
         query_list = model.query_music_table(title, year, artist)
-        print(query_list)
     user_dict = model.get_user_from_session(session)
     context = {'content': "Hello there", 'user': user_dict, 'session': session, 'query_list': query_list,
                'sub_list': sub_list, 'new_music': new_music}
+    app.logger.info('app.py main context: %s', context)
     app.logger.debug('app.py main END')
     return render_template('main.html', context=context)
 
@@ -90,57 +96,71 @@ def register():
         if not model.validate_user_email(request.form['email']):
             if model.create_new_user(request.form['email'], request.form['username'],
                                      request.form['password']):
-                print("New user created")
+                app.logger.debug('app.py register new user created')
                 app.logger.debug('app.py register END')
                 return redirect(url_for('login', error='New user created, try logging in'))
             else:
-                print("Unexpected error user creation")
+                app.logger.debug('app.py register Unexpected error user not created')
                 error = "Unexpected error user creation"
         else:
             error = "Email already exists"
     context = {'content': "Hello there", 'error': error}
+    app.logger.info('app.py register context:%s', context)
     app.logger.debug('app.py register END')
     return render_template('register.html', context=context)
 
 
 @app.route('/main/subscribe/<string:session>/<string:music>')
 def subscribe(session, music):
+    app.logger.debug('app.py subscribe BEGIN')
     error = "Logged out"
+    app.logger.info('app.py subscribe session, music: %s %s', session, music)
     if session is None:
         error = 'Please login first'
     if not model.validate_session(session) or music is None:
         error = 'Unauthenticated access detected, this user has been reported'
+        app.logger.debug('app.py subscribe Unauthenticated access detected, this user has been reported END')
         return redirect(url_for('login', error=error))
     if model.session_music_subscribe(session, music):
+        app.logger.debug('app.py subscribe END')
         return redirect(url_for('main', session=session, new_music='New music subscription available!'))
+    app.logger.debug('app.py subscribe redirect to login END')
     return redirect(url_for('login', error=error))
 
 
 @app.route('/main/unsubscribe/<string:session>/<string:music>')
 def unsubscribe(session, music):
+    app.logger.debug('app.py unsubscribe BEGIN')
     error = "Logged out"
     if session is None:
         error = 'Please login first'
     if not model.validate_session(session) or music is None:
         error = 'Unauthenticated access detected, this user has been reported'
+        app.logger.debug(
+            'app.py unsubscribe Unauthenticated access detected, this user has been reported END')
         return redirect(url_for('login', error=error))
     if model.session_music_unsubscribe(session, music):
+        app.logger.debug('app.py unsubscribe END')
         return redirect(url_for('main', session=session, new_music='Unsubscribed!'))
+    app.logger.debug('app.py unsubscribe redirect to login END')
     return redirect(url_for('login', error=error))
 
 
 @app.route('/logout', defaults={'session': None})
 @app.route('/logout/<string:session>')
 def logout(session):
+    app.logger.debug('app.py logout BEGIN %s', session)
     error = "Logged out"
     if session is None:
         error = 'Please login first'
     if not model.validate_session(session):
         error = 'Unauthenticated access detected, this user has been reported'
     model.remove_session(session)
+    app.logger.debug('app.py logout END %s', error)
     return redirect(url_for('login', error=error))
 
 
 if __name__ == '__main__':
+    app.logger.debug('app.py __main__ BEGIN')
     settings()
     app.run()
--- isai-flask/model.py
+++ isai-flask/model.py
@@ -5,6 +5,9 @@ import json
 import os
 import requests
 from boto3.dynamodb.conditions import Key, Attr
+from flask import Flask
+
+current_app = Flask(__name__)
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO,
@@ -22,24 +25,34 @@ session_dict = {}
 
 
 def init_boto3():
+    current_app.logger.debug('model.py init_boto3 BEGIN')
+    # current_app.logger.info('model.py init_boto3 error: %s', error)
     global dynamodb_client, dynamodb_resource, boto_init_status, existing_tables, s3_client, s3_resource
     dynamodb_client = boto3.client('dynamodb')
     dynamodb_resource = boto3.resource('dynamodb')
     s3_client = boto3.client('s3')
     s3_resource = boto3.resource('s3')
+    current_app.logger.info(
+        'model.py init_boto3 dynamodb_client, dynamodb_resource, s3_client, s3_resource: %s %s %s %s',
+        dynamodb_client, dynamodb_resource,
+        s3_client, s3_resource)
     check_existing_tables()
     boto_init_status = True
-    print("init_boto3 Existing tables: ", str(existing_tables))
     assert_dynamo()
+    current_app.logger.debug('model.py init_boto3 END')
     return True
 
 
 def check_existing_tables():
+    current_app.logger.debug('model.py check_existing_tables BEGIN')
     global existing_tables
     existing_tables = dynamodb_client.list_tables()['TableNames']
+    current_app.logger.info('model.py check_existing_tables error: %s', existing_tables)
+    current_app.logger.debug('model.py check_existing_tables END')
 
 
 def insert_defaults_login_table():
+    current_app.logger.debug('model.py insert_defaults_login_table BEGIN')
     for i in range(10):
         email = 's3813265' + str(i) + '@student.rmit.edu.au'
         user_name = 'abhis' + str(i)
@@ -65,10 +78,12 @@ def insert_defaults_login_table():
                 },
             }
         )
+    current_app.logger.debug('model.py insert_defaults_login_table END')
     return True
 
 
 def insert_defaults_music_table():
+    current_app.logger.debug('model.py insert_defaults_music_table BEGIN')
     f = open('a2.json')
     data = json.load(f)
     for item in data['songs']:
@@ -102,11 +117,14 @@ def insert_defaults_music_table():
                 },
             }
         )
+    current_app.logger.debug('model.py insert_defaults_music_table END')
     return True
 
 
 def download_all_images_from_json_to_bucket(url, uid):
+    current_app.logger.debug('model.py download_all_images_from_json_to_bucket BEGIN')
     response = requests.get(url)
+    current_app.logger.debug('model.py download_all_images_from_json_to_bucket %s', response)
     file = open("temp.jpg", "wb")
     file.write(response.content)
     file.close()
@@ -116,11 +134,13 @@ def download_all_images_from_json_to_bucket(url, uid):
 
 
 def assert_s3_bucket():
+    current_app.logger.debug('model.py assert_s3_bucket BEGIN')
     bucket_list = []
     for s in s3_client.list_buckets().get('Buckets'):
         bucket_list.append(s.get('Name'))
     if 'abhi-dev-music-images' in bucket_list:
-        print("S3 found!")
+        current_app.logger.debug('model.py assert_s3_bucket S3 found')
+        current_app.logger.debug('model.py assert_s3_bucket END')
         return True
     else:
         response = s3_client.create_bucket(
@@ -131,19 +151,20 @@ def assert_s3_bucket():
             },
             ObjectLockEnabledForBucket=False
         )
+        current_app.logger.debug('model.py assert_s3_bucket END')
         return True
 
 
 def assert_dynamo_login_table():
+    current_app.logger.debug('model.py assert_dynamo_login_table BEGIN')
     if not boto_init_status:
-        print("Unexpected boto_init_status")
+        current_app.logger.debug('model.py assert_dynamo_login_table Unexpected boto_init_status')
         return False
     table_name = 'login'
     global login_table
-    logger.info("assert_dynamo_login_table Start")
     check_existing_tables()
     if table_name not in existing_tables:
-        logger.info("assert_dynamo_login_table Table does not exist")
+        current_app.logger.debug('model.py assert_dynamo_login_table Table does not exist')
         login_table = dynamodb_resource.create_table(
             TableName='login',
             KeySchema=[
@@ -164,28 +185,31 @@ def assert_dynamo_login_table():
             }
         )
         dynamodb_client.get_waiter('table_exists').wait(TableName='login')
-        print(login_table)
+        current_app.logger.info('model.py assert_dynamo_login_table login table: %s', login_table)
         if login_table is None:
-            print("Unexpected failure, login table creation")
+            current_app.logger.debug(
+                'model.py assert_dynamo_login_table Unexpected failure, login table not created')
         else:
-            print("Table login is created")
+            current_app.logger.debug('model.py assert_dynamo_login_table Table login is created')
             insert_defaults_login_table()
     else:
         login_table = dynamodb_resource.Table(table_name)
-        logger.info("assert_dynamo_login_table login_table.table_status: ", login_table.table_status)
+        logger.info("model.py assert_dynamo_login_table login_table.table_status: %s",
+                    login_table.table_status)
+    current_app.logger.debug('model.py assert_dynamo_music_table END')
     return True
 
 
 def assert_dynamo_music_table():
+    current_app.logger.debug('model.py assert_dynamo_music_table BEGIN')
     if boto_init_status != True:
-        print("Unexpected boto_init_status")
+        current_app.logger.debug('model.py assert_dynamo_music_table Unexpected boto_init_status')
         return False
-    logger.info("assert_dynamo_music_table Start")
     table_name = 'music'
     global music_table
     check_existing_tables()
     if table_name not in existing_tables:
-        logger.info("assert_dynamo_music_table Table does not exist")
+        current_app.logger.debug('model.py assert_dynamo_music_table Table does not exist')
         music_table = dynamodb_resource.create_table(
             TableName='music',
             KeySchema=[
@@ -206,98 +230,127 @@ def assert_dynamo_music_table():
             }
         )
         dynamodb_client.get_waiter('table_exists').wait(TableName='music')
-        print(music_table)
+        current_app.logger.info('model.py assert_dynamo_music_table music table: %s', music_table)
         if music_table is None:
-            print("Unexpected failure, music table creation")
+            current_app.logger.debug(
+                'model.py assert_dynamo_music_table Unexpected failure, music table not created')
         else:
-            print("Table music is created")
+            current_app.logger.debug('model.py assert_dynamo_music_table music table created')
             insert_defaults_music_table()
     else:
         music_table = dynamodb_resource.Table(table_name)
-        print("Table status:", music_table.table_status)
+        current_app.logger.debug('model.py assert_dynamo_music_table music table status: %s',
+                                 music_table.table_status)
+        current_app.logger.debug('model.py assert_dynamo_music_table END')
     return True
 
 
 def assert_dynamo():
-    print("Assert Dynamo")
+    current_app.logger.debug('model.py assert_dynamo START')
     if not assert_dynamo_login_table():
-        print("Unexpected error, try again")
+        current_app.logger.debug('model.py assert_dynamo Unexpected error, try again END')
         return False
     if not assert_dynamo_music_table():
-        print("Unexpected error, try again")
+        current_app.logger.debug('model.py assert_dynamo Unexpected error, try again END')
         return False
     if not assert_s3_bucket():
-        print("Unexpected error, try again")
+        current_app.logger.debug('model.py assert_dynamo Unexpected error, try again END')
         return False
+    current_app.logger.debug('model.py assert_dynamo END')
     return True
 
 
 def validate_user_return_session(email, pswd):
+    current_app.logger.debug('model.py validate_user_return_session BEGIN')
+    current_app.logger.info('model.py validate_user_return_session email, pswd: %s %s', email, pswd)
     response = login_table.get_item(
         Key={
             'email': email,
         }
     )
-    print("Response", response)
+    current_app.logger.info('model.py validate_user_return_session response: %s', response)
     if 'Item' in response and response['Item']['pass'] == pswd:
         session_token = str(uuid.uuid1().hex)
+        current_app.logger.debug('model.py validate_user_return_session END')
         return store_session(session_token, response['Item'])
+    current_app.logger.debug('model.py validate_user_return_session END')
     return None
 
 
 def validate_user_email(email):
+    current_app.logger.debug('model.py validate_user_email BEGIN')
+    current_app.logger.info('model.py validate_user_email email: %s', email)
     response = login_table.get_item(
         Key={
             'email': email,
         }
     )
-    print("Response", response)
+    current_app.logger.info('model.py validate_user_email response: %s', response)
     if 'Item' in response:
-        print("Response item", response['Item'])
+        current_app.logger.info('model.py validate_user_email response item: %s', response['Item'])
+        current_app.logger.debug('model.py validate_user_email END')
         return True
+    current_app.logger.debug('model.py validate_user_email END')
     return False
 
 
 def store_session(session_id, session_details):
+    current_app.logger.debug('model.py store_session BEGIN')
+    current_app.logger.info('model.py store_session session_id, session_details: %s %s', session_id,
+                            session_details)
     global session_dict
     session_dict[session_id] = session_details
+    current_app.logger.info('model.py store_session session_dict[session_id]: %s', session_dict[session_id])
+    current_app.logger.debug('model.py store_session END')
     return session_id
 
 
 def refresh_session(session_id):
+    current_app.logger.debug('model.py refresh_session BEGIN')
     email = session_dict[session_id].get('email')
+    current_app.logger.info('model.py store_session session_id, email: %s %s', session_id, email)
     response = login_table.get_item(
         Key={
             'email': email,
         }
     )
-    print("Response", response)
+    current_app.logger.info('model.py refresh_session response: %s', response)
     if 'Item' in response:
         store_session(session_id, response['Item'])
+        current_app.logger.debug('model.py refresh_session END')
         return True
+    current_app.logger.debug('model.py refresh_session END')
     return False
 
 
 def remove_session(session_id):
+    current_app.logger.debug('model.py remove_session BEGIN')
     global session_dict
     session_dict.pop(session_id, None)
+    current_app.logger.debug('model.py remove_session END')
     return True
 
 
 def validate_session(session_id):
+    current_app.logger.debug('model.py validate_session')
     return session_id in session_dict
 
 
 def get_user_from_session(session_id):
+    current_app.logger.debug('model.py get_user_from_session BEGIN')
     if session_id not in session_dict:
+        current_app.logger.debug('model.py get_user_from_session END')
         return False
     else:
         new_dict = session_dict[session_id]
         new_dict.pop('pass', None)
+        current_app.logger.info('model.py get_user_from_session %s', new_dict)
+        current_app.logger.debug('model.py get_user_from_session END')
         return new_dict
 
 
 def create_new_user(email, username, pswd):
+    current_app.logger.debug('model.py create_new_user BEGIN')
     login_table.put_item(
         Item={
             'user_name': username,
@@ -305,24 +358,29 @@ def create_new_user(email, username, pswd):
             'pass': pswd,
         }
     )
+    current_app.logger.debug('model.py create_new_user END')
     return True
 
 
 def query_music_table(title, year, artist):
-    # if type(year) is not int:
-    #     return False
+    current_app.logger.debug('model.py query_music_table BEGIN')
+    current_app.logger.info('model.py query_music_table title, year, artist: %s %s %s', title, year, artist)
     year = year if year is None else int(year)
     response = music_table.scan(
         FilterExpression=(Attr('title').ne(title) if title is None else Attr('title').eq(title)) &
                          (Attr('year').ne(year) if year is None else Attr('year').eq(year)) &
                          (Attr('artist').ne(artist) if artist is None else Attr('artist').eq(artist))
     )
+    current_app.logger.debug('model.py query_music_table END')
     return response['Items']
 
 
 def session_music_subscribe(session, music):
+    current_app.logger.debug('model.py session_music_subscribe BEGIN')
     sub_list = get_sub_list(session)
     email = session_dict[session].get('email')
+    current_app.logger.debug('model.py session_music_subscribe session, music, email, sub_list: %s %s %s %s',
+                             session, music, email, sub_list)
     if sub_list is None:
         sub_list = []
     if music in sub_list:
@@ -337,15 +395,18 @@ def session_music_subscribe(session, music):
             ':s': sub_list,
         },
     )
-    print(response)
+    current_app.logger.info('model.py session_music_subscribe response: %s', response)
+    current_app.logger.debug('model.py session_music_subscribe END')
     return True
 
 
 def session_music_unsubscribe(session, music):
+    current_app.logger.debug('model.py session_music_unsubscribe BEGIN')
     sub_list = get_sub_list(session)
-    print(sub_list)
     email = session_dict[session].get('email')
-    print(email)
+    current_app.logger.debug(
+        'model.py session_music_unsubscribe session, music, email, sub_list: %s %s %s %s',
+        session, music, email, sub_list)
     if sub_list is None:
         return False
     if music in sub_list:
@@ -361,28 +422,26 @@ def session_music_unsubscribe(session, music):
             ':s': sub_list,
         },
     )
-    print(response)
+    current_app.logger.info('model.py session_music_unsubscribe response: %s', response)
+    current_app.logger.debug('model.py session_music_unsubscribe END')
     return True
 
 
 def get_sub_list(session):
+    current_app.logger.debug('model.py get_sub_list BEGIN')
     refresh_session(session)
     sub_list = session_dict.get(session).get('sub_list')
+    current_app.logger.debug('model.py get_sub_list END')
     return sub_list
 
 
 def get_music_dict_from_sub_id(uid):
+    current_app.logger.debug('model.py get_music_dict_from_sub_id BEGIN')
     response = music_table.get_item(
         Key={
             'id': uid,
         }
     )
-    # music_dict = {}
-    # music_dict['title'] = response.get('Item').get('title')
-    # music_dict['artist'] = response.get('Item').get('artist')
-    # music_dict['year'] = response.get('Item').get('year')
-    # music_dict['img_url'] = response.get('Item').get('img_url')
-    # music_dict['web_url'] = response.get('Item').get('web_url')
-    # print(music_dict)
-    print(response.get('Item'))
+    current_app.logger.info('model.py get_music_dict_from_sub_id response.get(Item):%s', response.get('Item'))
+    current_app.logger.debug('model.py get_music_dict_from_sub_id END')
     return response.get('Item')
